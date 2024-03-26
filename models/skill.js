c4dc319c4227f9fc801f45511e7e99c1c7e24692
Jason Nguyen <j1.nguyen@yahoo.com>
2024-03-26 15:19:20 -0700

    create skill model

---
+++ models/skill.js
@@ -0,0 +1,13 @@
+const skills = [
+    {id: 1254, name: "React"},
+    {id: 1891, name: "Django"},
+    {id: 1364, name: "Node"}
+];
+
+module.exports = {
+    getAll
+}
+
+function getAll() {
+    return skills;
+}
